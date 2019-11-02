a86fd4a3a2acf4f582c029f45b3ad8da75785bf3
Sam West <samdw05@gmail.com>
2019-11-02 21:49:16 +0000

    cleanup to actually works lol

--- cookieTrail/views.py
+++ cookieTrail/views.py
@@ -13,14 +13,8 @@ def index(request):
     chrome.Page.navigate(url="https://www.google.com/")
     chrome.wait_event("Page.frameStoppedLoading",timeout=60)
 
-    #time.sleep(2)
-
     cookies = chrome.Network.getCookies()
-    #request.session['uid'] = cookies
-    print(cookies)
-    #theUrl="http://127.0.0.1:8000/display?cook=".append(cookies)
     newstr = " ".join(("http://127.0.0.1:8000/displayCookies?cookieList=", str(cookies)))
-    print(newstr)
     chrome.Page.navigate(url=newstr)
 
     return HttpResponse("")
