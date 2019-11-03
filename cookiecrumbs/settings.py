259cdd36e0a60bbadc3f31408d3c85e1460d3ed1
njridsdale <njridsdale1@sheffield.ac.uk>
2019-11-03 08:13:07 +0000

    we nearly did it, and you bois left too early, come back and you can probs do the demo

--- cookiecrumbs/settings.py
+++ cookiecrumbs/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'm_wi@4zay2nq-_@@#1jo34b&5fly87q!ya-=6w%t+!8q@!!amn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-
+#ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = []
 TEMPLATE_DIRS = (
     os.path.join(SETTINGS_PATH, 'templates'),
 )
--- displayCookies/views.py
+++ displayCookies/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
+import http.cookies as cookies
+import json
 
 # Create your views here.
 def displayCookies(request):
     cookieList = request.GET.get('cookieList')
-    return HttpResponse("Cookie list: " + str(cookieList))
+    cookieObjects = []
+    cookieDump = eval(cookieList)
+
+    for cookie in cookieDump['result']['cookies']:
+       cookieObjects.append(cookie)
+      
+    #cookieObjects.append(cookieObjects)
+
+    cookieObjects.append('Test')
+
+    return render(request, 'cookieCrumbs/display.html', {'cookieObjects':cookieObjects})
