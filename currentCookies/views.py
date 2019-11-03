da688ee7baf039d2456c066dc86c5e00412d4fde
Twigonometry <mac.j.goodwin@gmail.com>
2019-11-03 08:20:27 +0000

    tidied settings and marked off current cookies as redundant (again)

--- currentCookies/views.py
+++ currentCookies/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+import PyChromeDevTools as pyChrome
+
+#might be good to use browsercookie library - functionality for returning local cookies on firefox and chrome
+#depends on a pycrypto library that requires C++, no time to install
+def currentCookies(request):
+    return HttpResponse("This page should display cookies stored locally in browser")
